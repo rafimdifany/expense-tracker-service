5f2e1f9fcfaaecae9b4a6dd4e748a7aac219229d
rafimdifany <rafimdifany@gmail.com>
2025-04-20 21:16:30 +0700

    initial commit

---
+++ src/main/java/com/expense_tracker/expense_tracker_service/ExpenseTrackerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.expense_tracker.expense_tracker_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseTrackerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseTrackerServiceApplication.class, args);
+	}
+
+}
