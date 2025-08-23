0e5fa709fcb5116ca46770af23fb64a947aa543c
rafimdifany <rafimdifany@gmail.com>
2025-08-23 21:10:43 +0700

    - add bearerAuth configuration on swagger - adjust dataseed import.sql

--- src/main/java/com/expense_tracker/expense_tracker_service/config/OpenAPIConfig.java
+++ src/main/java/com/expense_tracker/expense_tracker_service/config/OpenAPIConfig.java
@@ -1,9 +1,12 @@
 package com.expense_tracker.expense_tracker_service.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +15,7 @@ public class OpenAPIConfig {
 
     @Bean
     public OpenAPI expenseTranckerOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
         return new OpenAPI()
                 .info(new Info()
                         .title("Expense Tracker Service")
@@ -21,6 +25,14 @@ public class OpenAPIConfig {
                                 .email("rafimdifany@gmail.com")
                                 .name("rafimdifany")
                                 .url("https://github.com/rafimdifany"))
-                        .license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE")));
+                        .license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE")))
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
     }
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
 -- noinspection SqlNoDataSourceInspectionForFile
 
-insert into users (id, email, password, full_name) values (1, 'rafimdifany@gmail.com', '$2a$10$e/pZ4xeTZz9C3GG5oz9lquIm7TPHGqlIkye22mlz0vmvAFTT3dQoO', 'Rafi Mufadhal Difany')
+insert into users (email, password, full_name) values ('rafimdifany@gmail.com', '$2a$10$e/pZ4xeTZz9C3GG5oz9lquIm7TPHGqlIkye22mlz0vmvAFTT3dQoO', 'Rafi Mufadhal Difany')
